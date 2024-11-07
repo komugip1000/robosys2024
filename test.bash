91a92dfe7a89d0136770eded044eee9148639944
shota kaneko <komgip1000@gmail.com>
2024-11-07 12:13:48 +0900

    www

--- test.bash
+++ test.bash
@@ -10,14 +10,6 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
 
-###変な入力###
-out=$(seq あ | ./plus)
-[ "$?" = 0 ] || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
-
-ut=$(seq  | ./plus)
-[ "$?" = 0 ] || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
 
 [ "$res" = 0 ] && echo OK
-exit 0
+exit $res
