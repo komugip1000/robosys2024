4a13b59afc882e8595d1f2c997cafc740ed05c63
shota kaneko <komgip1000@gmail.com>
2024-11-28 22:02:16 +0900

    kari

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash  
+#!/bin/bash -xv
 
 ng () {
 	echo ${1}が違うよ
@@ -6,10 +6,34 @@ ng () {
 }
 res=0
 
-###正常石井###
+###plus###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
 
 
+
+###fibonacci###
+out=$(echo "3" | ./fibonacci)
+[ "$?" = 0 ] || ng "$LINENO"
+
+out=$(echo "530000" | ./fibonacci)
+[ "$?" = 0 ] || ng "$LINENO"
+
+out=$(echo "0" | ./fibonacci)
+[ "$?" = 0 ] || ng "$LINENO"
+
+out=$(echo "-3" | ./fibonacci)
+[ "$?" = 0 ] || ng "$LINENO"
+
+out=$(echo "3.5" | ./fibonacci)
+[ "$?" = 1 ] || ng "$LINENO"
+
+out=$(echo "a" | ./fibonacci)
+[ "$?" = 1 ] || ng "$LINENO"
+
+out=$(echo "?" | ./fibonacci)
+[ "$?" = 1 ] || ng "$LINENO"
+
 [ "$res" = 0 ] && echo OK
-exit $res
+
+exit "$res"
